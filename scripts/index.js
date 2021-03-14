8285874615a68753f1acbba1112f80ae80a381b5
Denis Myagotin <rexsatanas@yandex.ru>
2021-03-14 14:07:56 +0300

    done open/close

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,18 @@
-const popup = document.querySelector('popup');
+const popup = document.querySelector('.popup');
+const openPopupBtn = document.querySelector('.open_popup_btn');
+const closePopupBtn = document.querySelector('.popup__close-btn');
+console.log(closePopupBtn);
+
+function openPopup() {
+    popup.classList.add('popup_opened');  
+}
+
+function closePopup() {
+    popup.classList.remove('popup_opened');
+}
+
+openPopupBtn.addEventListener('click', function() {
+    openPopup();
+});
+
+closePopupBtn.addEventListener('click', function() {closePopup();});
